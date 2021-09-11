6e3a0780ce7c50dfc5dfd355b8fe88e793cf7a41
Kinayat Abay <74435594+AbayKinayat@users.noreply.github.com>
2021-09-11 23:38:56 +0600

    Todos components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { ListItemComponent } from './components/list-item/list-item.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     ListItemComponent,
+    TodosComponent,
   ],
   imports: [
     BrowserModule,
