0a490d5153c9846ba49a942492e788d49973ac8d
Adil-web <englishm224@gmail.com>
2021-09-11 23:08:45 +0600

    list item

---
+++ src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ListItem',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.css']
+})
+export class ListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
